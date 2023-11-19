ad756593c527baaef350e195e9f7546f71084478
PedroHNeves1 <pedrohneves2004@gmail.com>
2023-11-18 21:37:05 -0300

    commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
   $(window).scroll(function () {
-    if (this.scrolly > 20) {
+    if (this.scrollY > 20) {
       $("header").addClass("sticky");
     } else {
-      $("header").remove("sticky");
+      $("header").removeClass("sticky");
     }
   });
 });
